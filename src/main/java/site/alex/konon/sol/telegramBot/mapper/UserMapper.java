6b1c354b05819cdf36569424a11b5584f21b091c
yarrou <konic2006@yandex.ru>
2022-01-11 18:53:11 +0100

    changing the name of the UserFromRequest class to UserForm

--- src/main/java/site/alex/konon/sol/telegramBot/mapper/UserMapper.java
+++ src/main/java/site/alex/konon/sol/telegramBot/mapper/UserMapper.java
@@ -1,8 +1,8 @@
 package site.alex.konon.sol.telegramBot.mapper;
 
-import site.alex.konon.sol.telegramBot.dao.UserFromRequest;
+import site.alex.konon.sol.telegramBot.dao.UserForm;
 import site.alex.konon.sol.telegramBot.entity.User;
 
 public interface UserMapper {
-    User userFromRequestToUser(UserFromRequest dao);
+    User userFromRequestToUser(UserForm dao);
 }
--- src/main/java/site/alex/konon/sol/telegramBot/services/UserService.java
+++ src/main/java/site/alex/konon/sol/telegramBot/services/UserService.java
@@ -1,15 +1,15 @@
 package site.alex.konon.sol.telegramBot.services;
 
-import site.alex.konon.sol.telegramBot.dao.UserFromRequest;
+import site.alex.konon.sol.telegramBot.dao.UserForm;
 import site.alex.konon.sol.telegramBot.entity.User;
 
 public interface UserService {
-    User register(UserFromRequest userForm);
+    User register(UserForm userForm);
     User getUserById(long id);
     boolean isAdmin(User user);
     User save(User user);
     User getUserByLogin(String login);
-    User getUserFromRequest(UserFromRequest userForm);
-    User convertUserForm(UserFromRequest userForm);
+    User getUserFromRequest(UserForm userForm);
+    User convertUserForm(UserForm userForm);
     String loginUser(User user);
 }
