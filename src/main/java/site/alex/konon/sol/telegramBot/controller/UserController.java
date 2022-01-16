8d949d89974b99278e9ba21aa94016eebd52e0a8
yarrou <konic2006@yandex.ru>
2022-01-16 10:47:56 +0100

    the registration endpoint accepts the Device_used_language header to transmit the language used on the remote device

--- src/main/java/site/alex/konon/sol/telegramBot/controller/UserController.java
+++ src/main/java/site/alex/konon/sol/telegramBot/controller/UserController.java
@@ -1,5 +1,7 @@
 package site.alex.konon.sol.telegramBot.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import site.alex.konon.sol.telegramBot.dao.UserForm;
 import site.alex.konon.sol.telegramBot.entity.User;
 import site.alex.konon.sol.telegramBot.services.UserService;
+import javax.servlet.http.HttpServletRequest;
 
 
+@Slf4j
 @RestController
 public class UserController {
+    @Autowired
+    private HttpServletRequest request;
 
     private final UserService userService;
 
@@ -22,6 +28,10 @@ public class UserController {
 
     @PostMapping("/registration")
     public ResponseEntity registration(@RequestBody UserForm userForm){
+        String locale = getLanguage();
+        if(locale!=null){
+            userService.setLocale(locale);
+        }
         try {
             return new ResponseEntity(userService.register(userForm).getToken(), HttpStatus.OK);
         }catch (IllegalArgumentException e){
@@ -43,4 +53,9 @@ public class UserController {
             return new ResponseEntity(token,HttpStatus.OK);
         }
     }
+
+    private String getLanguage() {
+        return request.getHeader("Device_used_language");
+    }
+
 }
