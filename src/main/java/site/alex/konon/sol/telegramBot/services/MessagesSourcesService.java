cf2bd02e765c794c89ab765e1f8cf4f22e688264
yarrou <konic2006@yandex.ru>
2022-01-21 10:08:01 +0100

    MessagesSourcesService : added constants to indicate resources for sending e-mail messages about registration confirmation and successful completion of registration

--- src/main/java/site/alex/konon/sol/telegramBot/services/MessagesSourcesService.java
+++ src/main/java/site/alex/konon/sol/telegramBot/services/MessagesSourcesService.java
@@ -8,6 +8,13 @@ public interface MessagesSourcesService {
     public static final String SUGGESTION_CITY="bot.city.suggestion";
     public static final String LIST_CITY="bot.city.list";
     public static final String CLARIFY_CITY="bot.city.clarify";
+
+    public static final String EMAIL_REGISTRATION="email.registration";
+    public static final String EMAIL_REGISTRATION_SUBJECT="email.registration.subject";
+    public static final String EMAIL_CONFIRM_REGISTRATION="email.confirm.registration";
+    public static final String EMAIL_CONFIRM_REGISTRATION_SUBJECT="email.confirm.registration.subject";
+
+    public static final String MESSAGE_ALREADY_EXISTS="message.already.exists";
     public String getStringValue(String value);
     public String getStringValue(String value,String lang);
 }
