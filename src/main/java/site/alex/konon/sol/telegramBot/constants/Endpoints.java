0e2160cb8d1e263e87bce0fa3ab15e8e504ba870
yarrou <konic2006@yandex.ru>
2022-02-17 20:24:53 +0100

    Endpoints : added values for the endpoints of checking for updates and downloading the client application

--- src/main/java/site/alex/konon/sol/telegramBot/constants/Endpoints.java
+++ src/main/java/site/alex/konon/sol/telegramBot/constants/Endpoints.java
@@ -4,4 +4,6 @@ public class Endpoints {
     public static final String CONFIRM_REGISTRATION = "/confirm_registration";
     public static final String REGISTRATION = "/registration";
     public static final String LOGIN = "/login";
+    public static final String IS_UPDATE = "/update";
+    public static final String DOWNLOAD_APP = "/download";
 }
