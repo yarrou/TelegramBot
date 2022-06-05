9fe6783ea180210927d0d977c06d1fbb3403de3f
Yarrou <konic2884@gmail.com>
2022-06-05 15:46:02 +0200

    ImageFileServiceImpl : methods for encrypting and decrypting city images use new dependencies, city images are saved on the server as standard graphic files

--- src/main/java/site/alex/konon/sol/telegramBot/services/impl/ImageFileServiceImpl.java
+++ src/main/java/site/alex/konon/sol/telegramBot/services/impl/ImageFileServiceImpl.java
@@ -1,13 +1,16 @@
 package site.alex.konon.sol.telegramBot.services.impl;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import site.alex.konon.sol.telegramBot.entity.City;
 import site.alex.konon.sol.telegramBot.services.ImageFileService;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @Service
@@ -18,42 +21,52 @@ public class ImageFileServiceImpl implements ImageFileService {
 
     @Override
     public void writeImageFromString(City city) {
+        String base64String = decodeImage(city.getPicture());
+        String[] strings = base64String.split(",");
+        String extension;
+        switch (strings[0]) {//check image's extension
+            case "data:image/jpeg;base64":
+                extension = "jpeg";
+                break;
+            case "data:image/png;base64":
+                extension = "png";
+                break;
+            default:
+                extension = "jpg";
+                break;
+        }
+        String imagePath = pathImages + "/" + city.getName() + "." + extension;
+        byte[] decodedBytes = Base64.getDecoder().decode(city.getPicture());
         try {
-            String imagePath = pathImages + "/" + city.getName();
-            File image = new File(imagePath);
-
-            image.setWritable(true);
-            image.setReadable(true);
-            FileOutputStream file = new FileOutputStream(image);
-            file.write(city.getPicture().getBytes());
-            file.close();
-        } catch (Exception e) {
+            FileUtils.writeByteArrayToFile(new File(imagePath), decodedBytes);
+        } catch (IOException e) {
             log.error(e.getLocalizedMessage(), e);
+            throw new RuntimeException(e);
         }
     }
+
 
     @Override
     public String getImageAsString(City city) {
         try {
-            String imagePath = pathImages + "/" + city.getName();
-            File file = new File(imagePath);
-            FileInputStream imageInFile = new FileInputStream(file);
-            StringBuilder resultStringBuilder = new StringBuilder();
-            try (BufferedReader br
-                         = new BufferedReader(new InputStreamReader(imageInFile))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    resultStringBuilder.append(line).append("\n");
-                }
-            }
-            return resultStringBuilder.toString();
+            File cityImageFile;
+            String searchFileName = city.getName() + ".*";
+            File dirImages = new File(pathImages);
+            FileFilter fileFilter = new WildcardFileFilter(searchFileName);
+            File[] imagesArray = dirImages.listFiles(fileFilter);
+            if (imagesArray.length == 1){
+                cityImageFile = imagesArray[0];
+            } else throw new FileNotFoundException();
+            byte imageData[] = new FileInputStream(cityImageFile).readAllBytes();
+            return encodeImage(imageData);
         } catch (FileNotFoundException e) {
             try {
-                byte imageData[]  = getClass().getClassLoader().getResourceAsStream("static/images/city.png").readAllBytes();
+                byte imageData[] = getClass().getClassLoader().getResourceAsStream("static/images/city.png").readAllBytes();
                 String imageDataString = encodeImage(imageData);
                 city.setPicture(imageDataString);
                 return imageDataString;
             } catch (IOException ex) {
+                log.error(e.getLocalizedMessage(), e);
                 throw new RuntimeException(ex);
             }
         } catch (IOException e) {
@@ -61,11 +74,12 @@ public class ImageFileServiceImpl implements ImageFileService {
             return "";
         }
     }
+
     private String encodeImage(byte[] imageByteArray) {
-        return Base64.encodeBase64String(imageByteArray);
+        return Base64.getEncoder().encodeToString(imageByteArray);
     }
 
-    private byte[] decodeImage(String imageDataString) {
-        return Base64.decodeBase64(imageDataString);
+    private String decodeImage(String imageDataString) {
+        return new String(Base64.getDecoder().decode(imageDataString), StandardCharsets.UTF_8);
     }
 }
