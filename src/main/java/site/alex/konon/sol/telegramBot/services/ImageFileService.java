a4a9968439ca0733b09b35c574520a4284496392
Yarrou <konic2884@gmail.com>
2022-06-05 20:54:07 +0200

    ImageFileService : added method returning city image file

--- src/main/java/site/alex/konon/sol/telegramBot/services/ImageFileService.java
+++ src/main/java/site/alex/konon/sol/telegramBot/services/ImageFileService.java
@@ -2,7 +2,10 @@ package site.alex.konon.sol.telegramBot.services;
 
 import site.alex.konon.sol.telegramBot.entity.City;
 
+import java.io.File;
+
 public interface ImageFileService {
     void writeImageFromString(City city);
     String getImageAsString(City city);
+    File getImagePath(City city);
 }
