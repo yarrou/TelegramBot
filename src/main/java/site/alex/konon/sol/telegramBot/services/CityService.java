22fe244baa008c2b96b3c01c67ef179fd3206b26
Yarrou <konic2884@gmail.com>
2022-06-28 13:06:15 +0200

    CityService : new methods for checking the availability of the City in the database, as well as for saving an object of the City type along with its image

--- src/main/java/site/alex/konon/sol/telegramBot/services/CityService.java
+++ src/main/java/site/alex/konon/sol/telegramBot/services/CityService.java
@@ -1,5 +1,6 @@
 package site.alex.konon.sol.telegramBot.services;
 
+import org.springframework.web.multipart.MultipartFile;
 import site.alex.konon.sol.telegramBot.entity.City;
 
 import java.util.List;
@@ -7,8 +8,9 @@ import java.util.List;
 public interface CityService {
     List<String> findCities(String name);
     City getCityByName(String name);
-    List<City> findCity(String name);
     boolean addCity(City city);
     boolean changeCity(City city);
     boolean deleteCity(String name);
+    void saveCity(City city,MultipartFile file);
+    boolean isExist(City city);
 }
