02060982173fc26a842db854e651594b4a11f7f6
Yarrou <konic2884@gmail.com>
2022-07-03 07:29:10 +0200

    ImageFileService : added a method that saves a file to the cloud and a method that allows you to get a file from the cloud

--- src/main/java/site/alex/konon/sol/telegramBot/services/ImageFileService.java
+++ src/main/java/site/alex/konon/sol/telegramBot/services/ImageFileService.java
@@ -1,6 +1,7 @@
 package site.alex.konon.sol.telegramBot.services;
 
 import org.springframework.web.multipart.MultipartFile;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import site.alex.konon.sol.telegramBot.entity.City;
 
 import java.io.File;
@@ -9,6 +10,9 @@ public interface ImageFileService {
     File getImageFile(City city);
 
     String saveImage(City city, MultipartFile file);
+    String saveImageInS3(City city,MultipartFile file);
 
     byte[] getImageAsByteArray(String imageName);
+
+    InputFile getInputFile(City city);
 }
