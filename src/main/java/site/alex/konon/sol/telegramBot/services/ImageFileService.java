adb449659637e61600c4fad1984ebce8e654e4c2
Yarrou <konic2884@gmail.com>
2022-07-03 20:40:07 +0200

    ImageFileService : deleted unused methods

--- src/main/java/site/alex/konon/sol/telegramBot/services/ImageFileService.java
+++ src/main/java/site/alex/konon/sol/telegramBot/services/ImageFileService.java
@@ -4,15 +4,9 @@ import org.springframework.web.multipart.MultipartFile;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import site.alex.konon.sol.telegramBot.entity.City;
 
-import java.io.File;
-
 public interface ImageFileService {
-    File getImageFile(City city);
 
-    String saveImage(City city, MultipartFile file);
-    String saveImageInS3(City city,MultipartFile file);
-
-    byte[] getImageAsByteArray(String imageName);
+    String saveImageInS3(City city, MultipartFile file);
 
     InputFile getInputFile(City city);
 }
